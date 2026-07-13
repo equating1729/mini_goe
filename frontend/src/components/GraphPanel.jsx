05b8011834f896cfb2ae6a719c77d5112577cd4a
Ishika Garg <gargishika1213@gmail.com>
2026-07-13 14:29:20 +0530

    updated frontend with supabase database

--- frontend/src/components/GraphPanel.jsx
+++ frontend/src/components/GraphPanel.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const API = "http://localhost:8000/api";
+const API = import.meta.env.VITE_API;
 
 export default function GraphPanel() {
   const [entities, setEntities] = useState([]);
