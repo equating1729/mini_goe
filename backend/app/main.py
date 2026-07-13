e0ed43e8ed2894cd24a3bcd8f62f9fa4502811bd
Ishika Garg <gargishika1213@gmail.com>
2026-07-13 12:45:39 +0530

    cors update for vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,9 @@ app = FastAPI(title="Mini GOE API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+        "https://mini-goe.vercel.app",
+        "https://*.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
